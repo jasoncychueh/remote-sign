d2eb9e757e5dce53bffca65a22b723d0101d182d
JasonCYChueh <JasonCYChueh@fih-foxconn.com>
2016-11-17 15:11:23 +0800

    1. Add connection timeout for signer. 2. Ready to release.

--- src/main/groovy/com/fihtdc/gradle/remotesign/RemoteSigner.java
+++ src/main/groovy/com/fihtdc/gradle/remotesign/RemoteSigner.java
@@ -17,6 +17,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Credentials;
 import okhttp3.MediaType;
@@ -29,7 +30,7 @@ import okhttp3.Response;
 /**
  * Created by Jason on 11/12/2016.
  */
-class RemoteSigner {
+public class RemoteSigner {
     private static final Logger LOGGER = LoggerFactory.getLogger(RemoteSigner.class);
 
     private static final String REQUEST_URL = "http://%s:%d/Jenkins/job/Remote_APK_CERT_TOOL/buildWithParameters"
@@ -42,25 +43,24 @@ class RemoteSigner {
 
     private static final String DB_URL = "jdbc:mysql://%s/Jenkins";
 
-    static {
-        try {
-            Class.forName(JDBC_DRIVER);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
     private OkHttpClient mClient;
 
     private RemoteSigningConfig mConfigs;
 
     private Logger mLogger = LOGGER;
 
-    RemoteSigner(Logger logger, RemoteSigningConfig configs) {
+    public RemoteSigner(Logger logger, RemoteSigningConfig configs) {
         mLogger = logger;
         mConfigs = configs;
 
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            mLogger.error(e.getMessage(), e);
+        }
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .connectTimeout(5, TimeUnit.SECONDS);
 
         try {
             String proxyHost = System.getProperty("http.proxyHost", null);
@@ -69,6 +69,7 @@ class RemoteSigner {
             String proxyPassword = System.getProperty("http.proxyPassword", null);
 
             if (proxyHost != null) {
+                mLogger.info("Found proxy '{}' in gradle settings.", proxyHost);
                 Proxy proxy = new Proxy(Proxy.Type.HTTP,
                         new InetSocketAddress(proxyHost, proxyPort));
                 builder.proxy(proxy);
@@ -76,6 +77,7 @@ class RemoteSigner {
                 if (proxyUser != null && proxyPassword != null) {
                     builder.proxyAuthenticator((route, response) -> {
                         String credential = Credentials.basic(proxyUser, proxyPassword);
+                        mLogger.info("Use proxy credential {}", credential);
                         return response.request().newBuilder()
                                 .header("Proxy-Authorization", credential)
                                 .build();
@@ -116,16 +118,12 @@ class RemoteSigner {
             Response response = mClient.newCall(request).execute();
             mLogger.debug("response={}", response);
             if (response.code() == 201) {
-                try {
-                    int jobId = getJobId(reqId, "Remote_APK_CERT_TOOL");
-                    LOGGER.debug("jobId={}", jobId);
-                    downloadApk(jobId, apkFile);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+                int jobId = getJobId(reqId, "Remote_APK_CERT_TOOL");
+                LOGGER.debug("jobId={}", jobId);
+                downloadApk(jobId, apkFile);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException | SQLException e) {
+            mLogger.error(e.getMessage(), e);
         }
     }
 
@@ -162,20 +160,21 @@ class RemoteSigner {
             apkFile.delete();
             outFile.renameTo(apkFile);
         } catch (IOException e) {
-            e.printStackTrace();
+            mLogger.error(e.getMessage(), e);
         }
     }
 
     private int getJobId(long reqId, String jobName) throws SQLException {
         String url = String.format(Locale.getDefault(), DB_URL, mConfigs.getServerHost());
         mLogger.debug("dbUrl={}", url);
-        String sql = "select job_id from job_status where job_name='" + jobName
-                + "' and request_id='" + reqId + "'";
+
+        String sql = "select job_id from job_status where job_name='%s' and request_id='%d'";
+        sql = String.format(Locale.getDefault(), sql, jobName, reqId);
+        mLogger.debug("sql={}", sql);
         Connection conn = DriverManager.getConnection(url, mConfigs.getDbUser(),
                 mConfigs.getDbPassword());
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(sql);
-        mLogger.debug("stmt={}", stmt);
         int jobId = rs.next() ? rs.getInt(1) : -1;
         // STEP 6: Clean-up environment
         rs.close();
